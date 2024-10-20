5f2dc33e0b53083a7049e4249ce4ecf8a4435b3f
iddob <iddob@DESKTOP-UP92I9T>
2024-10-20 16:41:47 +0300

    Added Sample class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opencv/DetectSamples.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opencv/DetectSamples.java
@@ -37,20 +37,19 @@ public class DetectSamples extends OpenCvPipeline {
 
         List<MatOfPoint> contours = new ArrayList<>();
         Imgproc.findContours(mask(input), contours, new Mat(), Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
+        List<Sample> samples = new ArrayList<>();
 
         for (MatOfPoint contour : contours) {
+            //check if contour is a valid sample
+            if (contour.size().area() < 500 || contour.size().area() > 5000) //TODO: figure out what these constants should be
+                continue;
+
             MatOfPoint2f contour2f = new MatOfPoint2f(contour.toArray());
 
             double epsilon = epsilonConstant * Imgproc.arcLength(contour2f, true);
             Imgproc.approxPolyDP(contour2f, contour2f, epsilon, true);
             Point[] vertices = contour2f.toArray();
-
-            double[] coordinates = getCoordinates(vertices);
-            telemetry.addData("x", coordinates[0]);
-            telemetry.addData("y", coordinates[1]);
-            double orientation = calculateOrientation(vertices);
-            //Imgproc.putText(input, Math.round(coordinates[0]) + "," + Math.round(coordinates[1]), vertices[0], Imgproc.FONT_HERSHEY_SIMPLEX, 0.3, new Scalar(0, 0, 0), 1);
-            Imgproc.putText(input, "" + Math.round(Math.toDegrees(orientation)), vertices[0], Imgproc.FONT_HERSHEY_SIMPLEX, 0.3, new Scalar(70, 0, 0), 1);
+            samples.add(new Sample(vertices));
         }
         telemetry.update();
         return input;
@@ -65,37 +64,6 @@ public class DetectSamples extends OpenCvPipeline {
         Imgproc.morphologyEx(masked, masked, Imgproc.MORPH_OPEN, kernel);
         Imgproc.morphologyEx(masked, masked, Imgproc.MORPH_CLOSE, kernel);
         return masked;
-    }
-
-    public double[] getCoordinates(Point[] vertices) {
-        Point reference = vertices[0];
-        for (Point vertex : vertices) {
-            if (vertex.y > reference.y) {
-                reference = vertex;
-            }
-        }
-        double y = Camera.z / Math.tan(Math.toRadians((reference.y - Camera.halfImageHeight) * Camera.vOVERheight));
-        return new double[] {Math.tan(Math.toRadians((reference.x - Camera.halfImageWidth) * Camera.hOVERwidth)) * y, y};
-    }
-
-    public double calculateOrientation(Point[] vertices) {
-        int index = 0;
-        for (int i = 0; i < vertices.length; i++) {
-            if (vertices[i].y < vertices[index].y) {
-                index = i;
-            }
-        }
-        Point point1 = vertices[(index - 1 + vertices.length) % vertices.length];
-        Point point2 = vertices[index];
-        Point point3 = vertices[(index + 1) % vertices.length];
-        double verticalAngle = (point2.y - Camera.halfImageHeight) * Camera.vOVERheight;
-        double wanted_length_squared = Math.pow(point3.y - point2.y, 2) + Math.pow(point2.x - point3.x, 2);
-        telemetry.addData("Length 2-3", wanted_length_squared);
-        double other_length_squared = Math.pow(point1.y - point2.y, 2) + Math.pow(point2.x - point1.x, 2);
-        telemetry.addData("Length 1-2", other_length_squared);
-        if (other_length_squared < wanted_length_squared)
-            point1 = point3;
-        return Math.atan((point1.y - point2.y) / ((point2.x - point1.x) * Math.tan(Math.toRadians(verticalAngle))));
     }
 
 
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opencv/Sample.java
@@ -0,0 +1,43 @@
+package org.firstinspires.ftc.teamcode.opencv;
+import org.opencv.core.Point;
+public class Sample {
+    private double sampleX, sampleY, orientation;
+    private Point[] vertices;
+    public Sample(Point[] vertices) {
+        this.vertices = vertices;
+        calculatePosition();
+        calculateOrientation();
+    }
+    private double getSampleX() {return sampleX;}
+    private double getSampleY() {return sampleY;}
+    private double getOrientation() {return orientation;}
+
+    public void calculatePosition() {
+        Point reference = vertices[0];
+        for (Point vertex : vertices) {
+            if (vertex.y > reference.y) {
+                reference = vertex;
+            }
+        }
+        double y = Camera.z / Math.tan(Math.toRadians((reference.y - Camera.halfImageHeight) * Camera.vOVERheight));
+        sampleX = Math.tan(Math.toRadians((reference.x - Camera.halfImageWidth) * Camera.hOVERwidth)) * y;
+        sampleY = y;
+    }
+    private void calculateOrientation() {
+        int index = 0;
+        for (int i = 0; i < vertices.length; i++) {
+            if (vertices[i].y < vertices[index].y) {
+                index = i;
+            }
+        }
+        Point point1 = vertices[(index - 1 + vertices.length) % vertices.length];
+        Point point2 = vertices[index];
+        Point point3 = vertices[(index + 1) % vertices.length];
+        double verticalAngle = (point2.y - Camera.halfImageHeight) * Camera.vOVERheight;
+        double wanted_length_squared = Math.pow(point3.y - point2.y, 2) + Math.pow(point2.x - point3.x, 2);
+        double other_length_squared = Math.pow(point1.y - point2.y, 2) + Math.pow(point2.x - point1.x, 2);
+        if (other_length_squared < wanted_length_squared)
+            point1 = point3;
+        orientation =  Math.atan((point1.y - point2.y) / ((point2.x - point1.x) * Math.tan(Math.toRadians(verticalAngle))));
+    }
+}
