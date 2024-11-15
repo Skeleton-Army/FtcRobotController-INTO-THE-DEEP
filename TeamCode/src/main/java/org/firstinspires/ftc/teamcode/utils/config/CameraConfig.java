7cc9e95829df9535272f801971673b0c06af841e
BuildTools <unconfigured@null.spigotmc.org>
2024-11-15 17:21:36 +0200

    Added @Config to CameraConfig

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/config/CameraConfig.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/config/CameraConfig.java
@@ -1,11 +1,14 @@
 package org.firstinspires.ftc.teamcode.utils.config;
 
-public final class CameraConfig {
-    public static final float z = 11.4f; //TODO: figure out what this is
-    public static final float hOVERwidth = 0.24375f;
-    public static final float vOVERheight = 0.1725f;
-    public static final int halfImageWidth = 160;
-    public static final int halfImageHeight = 120;
-    public static final double offsetX = 6.6;
-    public static final double offsetY = 8.2;
+import com.acmerobotics.dashboard.config.Config;
+
+@Config
+public class CameraConfig {
+    public static float z = 11.4f; //TODO: figure out what this is
+    public static float hOVERwidth = 0.24375f;
+    public static float vOVERheight = 0.1725f;
+    public static int halfImageWidth = 160;
+    public static int halfImageHeight = 120;
+    public static double offsetX = 6.6;
+    public static double offsetY = 8.2;
 }
