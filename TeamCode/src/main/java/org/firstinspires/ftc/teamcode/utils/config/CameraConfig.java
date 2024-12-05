bd231e8dedec6c0c357555af773db81249d57a4c
angrybirdsFTC <angrybirdsftc@gmail.com>
2024-12-05 19:41:11 +0200

    added something extra to the SampleTest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/config/CameraConfig.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/config/CameraConfig.java
@@ -4,11 +4,11 @@ import com.acmerobotics.dashboard.config.Config;
 
 @Config
 public class CameraConfig {
-    public static float z = 13.25f; //TODO: figure out what this is
-    public static float horizontalFOV = 64.4f; // Larger - more to the right
-    public static float verticalFOV = 46.8f; // Larger - more backwards
-    public static int halfImageWidth = 160;
-    public static int halfImageHeight = 120;
+    public static float z = 13.125f;
+    public static float horizontalFOV = 63.62f; // needs tuning
+    public static float verticalFOV = 35.78f; // needs tuning
+    public static int halfImageWidth = 320;
+    public static int halfImageHeight = 180;
     public static double offsetX = 0; //x is horizontal not IDO-style, -is when overshooting (it passes it = backwards)
     public static double offsetY = 0;
     public static double offsetHorizontal = 0;
