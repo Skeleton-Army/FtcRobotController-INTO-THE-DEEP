73e7057239bea59c49a8263fe66b113be8e6ae6d
angrybirdsFTC <angrybirdsftc@gmail.com>
2024-12-19 17:04:03 +0200

    Fixed AutonomousPoseTool

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/tests/teleop/AutonomousPoseTool.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/tests/teleop/AutonomousPoseTool.java
@@ -7,6 +7,7 @@ import org.firstinspires.ftc.teamcode.opModes.TeleopApplication;
 import org.firstinspires.ftc.teamcode.utils.general.Debounce;
 import org.firstinspires.ftc.teamcode.utils.general.PoseStorage;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -29,15 +30,23 @@ public class AutonomousPoseTool extends TeleopApplication {
 
     @Override
     public void loop() {
-        telemetry.addData("Current Pose", drive.pose);
+        DecimalFormat f = new DecimalFormat("##.0");
+
+        telemetry.addData("Current X", f.format(drive.pose.position.x));
+        telemetry.addData("Current Y", f.format(drive.pose.position.y));
+        telemetry.addData("Current Heading", f.format(Math.toDegrees(drive.pose.heading.toDouble())));
 
         if (Debounce.isButtonPressed("a", gamepad1.a)) {
             savedPoses.add(drive.pose);
         }
 
         for (int i = 0; i < savedPoses.size(); i++) {
-            telemetry.addData(String.valueOf(i), savedPoses.get(i));
+            telemetry.addData(i + " X", f.format(savedPoses.get(i).position.x));
+            telemetry.addData(i + " Y", f.format(savedPoses.get(i).position.y));
+            telemetry.addData(i + " Heading", f.format(Math.toDegrees(savedPoses.get(i).heading.toDouble())));
         }
+
+        telemetry.addLine();
 
         super.loop();
     }
