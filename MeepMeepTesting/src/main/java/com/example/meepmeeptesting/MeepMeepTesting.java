fcb2d548f81781f0ac06a77d25c708b4fd3f8d46
AmiT1458 <97545597+AmiT1458@users.noreply.github.com>
2024-12-21 21:11:08 +0200

    experimenting with the spline thingy

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -1,7 +1,10 @@
 package com.example.meepmeeptesting;
 
 import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.QuinticSpline1d;
+import com.acmerobotics.roadrunner.QuinticSpline2d;
 import com.acmerobotics.roadrunner.Rotation2d;
+import com.acmerobotics.roadrunner.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.Constraints;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
@@ -22,11 +25,21 @@ public class MeepMeepTesting {
 
         CustomDriveShim customDriveShim = convertToCustomDriveShim(myBot.getDrive(), constraints);
 
-        myBot.runAction(customDriveShim.actionBuilder(new Pose2d(20, -56, Math.toRadians(90.00)), false)
+        QuinticSpline1d splinex = new QuinticSpline1d(287.9999999999997, -719.9999999999994, 479.9999999999996, 0.0, 0.0, -24.0);
+        QuinticSpline1d spliney = new QuinticSpline1d(221.99999999999983, -554.9999999999995, 369.99999999999966, 0.0, 0.0, -37.0);
+
+        QuinticSpline2d  spline = new QuinticSpline2d(splinex, spliney);
+
+        Vector2d v1 = spline.get(1, 1).value();
+
+        myBot.runAction(customDriveShim.actionBuilder(new Pose2d(-24, 50, Math.toRadians(0)), false)
                 //.splineTo(new Vector2d(10, -35), Math.PI / 2, null, new ProfileAccelConstraint(-100, 100))
-                .splineToLinearHeading(new Pose2d(-24,0,Math.toRadians(0)), new Rotation2d(2.2,1.3))
+                .splineTo(v1, new Rotation2d(Math.toRadians(-180),Math.toRadians(-25)))
+//                .splineToConstantHeading(v1, Math.toRadians(90))
                 .build()
         );
+
+        System.out.println(v1);
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_JUICE_DARK)
                 .setDarkMode(true)
