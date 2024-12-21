956cb53e995ce5fae946b5dac1e44c44b83a7a37
BuildTools <unconfigured@null.spigotmc.org>
2024-12-21 20:56:11 +0200

    Improved the FSM and added incomplete basket strategy

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/AutoApplication.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/AutoApplication.java
@@ -14,38 +14,59 @@ import org.firstinspires.ftc.teamcode.utils.autonomous.AutoOpMode;
 import org.firstinspires.ftc.teamcode.utils.general.Utilities;
 import org.firstinspires.ftc.teamcode.utils.general.prompts.OptionPrompt;
 
-import java.util.List;
+enum Alliance {
+    RED,
+    BLUE
+}
+
+enum Strategy {
+    SPECIMENS,
+    BASKET
+}
 
 @Autonomous(name = "Autonomous App", group = "SA_FTC", preselectTeleOp = "Teleop App")
 public class AutoApplication extends AutoOpMode {
-    public enum State {
+    protected enum State {
         HANG_SPECIMEN,
-        PICKUP_SPECIMEN,
-        COLLECT_SAMPLES
+        COLLECT_ADDITIONAL_SAMPLE,
+
+        COLLECT_COLOR_SAMPLES,
+        COLLECT_SPECIMEN,
+
+        COLLECT_YELLOW_SAMPLE,
+        PUT_IN_BASKET,
+
+        PARK
     }
 
     Intake intake;
     Outtake outtake;
 
-    String alliance;
+    Alliance alliance;
+    Strategy strategy;
+
+    double startingXPos = 0;
+    int collectedSamples = 0;
 
     @Override
     protected State initialState() {
-        return State.COLLECT_SAMPLES;
+        return State.HANG_SPECIMEN;
     }
 
     @Override
     protected void registerStates() {
         addState(State.HANG_SPECIMEN, this::hangSpecimen);
-        addState(State.PICKUP_SPECIMEN, this::pickupSpecimen);
-        addState(State.COLLECT_SAMPLES, this::collectSamples);
+        addState(State.COLLECT_YELLOW_SAMPLE, this::collectYellowSample);
+        addState(State.PUT_IN_BASKET, this::putInBasket);
+        addState(State.PARK, this::park);
+//        addState(State.COLLECT_SPECIMEN, this::pickupSpecimen);
+//        addState(State.COLLECT_COLOR_SAMPLES, this::collectColorSamples);
     }
 
     @Override
     public void setPrompts() {
         choiceMenu.enqueuePrompt(new OptionPrompt("alliance", "SELECT AN ALLIANCE:", "Red", "Blue"));
-        choiceMenu.enqueuePrompt(new OptionPrompt("position", "SELECT THE STARTING POSITION:", "Observation Zone Side", "Basket Side"));
-        choiceMenu.enqueuePrompt(new OptionPrompt("strategy", "SELECT A STRATEGY:", "Specimens", "Yellow Samples"));
+        choiceMenu.enqueuePrompt(new OptionPrompt("strategy", "SELECT A STRATEGY:", "Specimens", "Basket"));
         choiceMenu.enqueuePrompt(new OptionPrompt("specimens", "SELECT HUMAN PLAYER SPECIMENS:", "0", "1"));
     }
 
@@ -61,59 +82,134 @@ public class AutoApplication extends AutoOpMode {
 
     @Override
     public void start() {
-        super.start();
-
         // Enable auto bulk reads
         Utilities.setBulkReadsMode(hardwareMap, LynxModule.BulkCachingMode.AUTO);
 
-        alliance = choiceMenu.getValueOf("alliance").toString();
-        String position = choiceMenu.getValueOf("position").toString();
-        String strategy = choiceMenu.getValueOf("strategy").toString();
-        String specimens = choiceMenu.getValueOf("specimens").toString();
+        // Fetch choices
+        String selectedAlliance = choiceMenu.getValueOf("alliance").toString();
+        String selectedStrategy = choiceMenu.getValueOf("strategy").toString();
+        String selectedSpecimens = choiceMenu.getValueOf("specimens").toString();
 
-        telemetry.addData("Selected Alliance", alliance);
-        telemetry.addData("Selected Position", position);
-        telemetry.addData("Selected Strategy", strategy);
-        telemetry.addData("Selected Specimen", specimens);
+        telemetry.addData("Selected Alliance", selectedAlliance);
+        telemetry.addData("Selected Strategy", selectedStrategy);
+        telemetry.addData("Selected Specimen", selectedSpecimens);
 
-        drive.pose = new Pose2d(10, -61.5, Math.toRadians(90.00));
+        // Initialize values
+        alliance = selectedAlliance.equals("Red") ? Alliance.RED : Alliance.BLUE;
+        strategy = selectedStrategy.equals("Specimens") ? Strategy.SPECIMENS : Strategy.BASKET;
+
+        switch (strategy) {
+            case SPECIMENS:
+                startingXPos = 10;
+                break;
+            case BASKET:
+                startingXPos = -10;
+                break;
+        }
+
+        // Set starting position
+        drive.pose = new Pose2d(startingXPos, -61.5, Math.toRadians(90.00));
     }
 
-    private void collectSamples() {
-        Actions.runBlocking(
-                drive.actionBuilder(drive.pose, alliance.equals("Blue"))
-                        .splineTo(new Vector2d(10, -35), Math.PI / 2, null, new ProfileAccelConstraint(-50, 100))
-                        .splineToSplineHeading(new Pose2d(17.07, -49.05, Math.toRadians(90)), Math.toRadians(-14.83))
-                        .splineToLinearHeading(new Pose2d(48, -43, Math.toRadians(90)), Math.toRadians(67.62))
-                        .turnTo(Math.toRadians(60))
-                        .waitSeconds(0.5)
-                        .turnTo(Math.toRadians(90))
-                        .turnTo(Math.toRadians(40))
-                        .waitSeconds(0.5)
-                        .turnTo(Math.toRadians(90))
-                        .build()
-        );
-
-        setState(State.PICKUP_SPECIMEN);
-    }
+    // -------------- States --------------
 
     private void hangSpecimen() {
         Actions.runBlocking(
-                drive.actionBuilder(drive.pose, alliance.equals("Blue"))
-                        .splineTo(new Vector2d(10, -35), Math.PI / 2, null, new ProfileAccelConstraint(-50, 100))
+                drive.actionBuilder(drive.pose, alliance == Alliance.BLUE)
+                        .splineTo(new Vector2d(startingXPos, -35), Math.PI / 2, null, new ProfileAccelConstraint(-50, 100))
                         .build()
         );
 
-        setState(State.PICKUP_SPECIMEN);
+        addConditionalTransition(strategy == Strategy.SPECIMENS, State.COLLECT_COLOR_SAMPLES, State.COLLECT_YELLOW_SAMPLE);
     }
 
-    private void pickupSpecimen() {
+    private void collectYellowSample() {
+        collectedSamples++;
+
+        switch (collectedSamples) {
+            case 1:
+                // Collect first sample
+                Actions.runBlocking(
+                        drive.actionBuilder(drive.pose, alliance == Alliance.BLUE)
+                                .splineToConstantHeading(new Vector2d(10, -50), Math.PI / 2)
+                                .splineTo(new Vector2d(-55, -48), Math.toRadians(75))
+                                .build()
+                );
+                break;
+            case 2:
+                // Collect second sample
+                Actions.runBlocking(
+                        drive.actionBuilder(drive.pose, alliance == Alliance.BLUE)
+                                .splineTo(new Vector2d(-60, -48), Math.toRadians(90))
+                                .build()
+                );
+                break;
+            case 3:
+                // Collect third sample
+                Actions.runBlocking(
+                        drive.actionBuilder(drive.pose, alliance == Alliance.BLUE)
+                                .splineTo(new Vector2d(-60, -48), Math.toRadians(105))
+                                .build()
+                );
+                break;
+        }
+
+        addTransition(State.PUT_IN_BASKET);
+    }
+
+    private void putInBasket() {
+        // Put the sample in the basket
         Actions.runBlocking(
-                drive.actionBuilder(drive.pose, alliance.equals("Blue"))
-                        .splineToLinearHeading(new Pose2d(35, -55, Math.toRadians(-90)), Math.toRadians(180))
+                drive.actionBuilder(drive.pose, alliance == Alliance.BLUE)
+                        .splineToLinearHeading(new Pose2d(-55, -55, Math.toRadians(225)), Math.PI / 2)
                         .build()
         );
 
-        setState(State.HANG_SPECIMEN);
+        addConditionalTransition(collectedSamples < 3, State.COLLECT_YELLOW_SAMPLE, State.PARK);
     }
+
+    private void park() {
+        switch (strategy) {
+            case SPECIMENS:
+                // Park in observation zone
+                Actions.runBlocking(
+                        drive.actionBuilder(drive.pose, alliance == Alliance.BLUE)
+                                .splineTo(new Vector2d(50, -60), Math.toRadians(0))
+                                .build()
+                );
+                break;
+            case BASKET:
+                // Park at bars
+                Actions.runBlocking(
+                        drive.actionBuilder(drive.pose, alliance == Alliance.BLUE)
+                                .turnTo(Math.toRadians(90))
+                                .splineTo(new Vector2d(-25, -10), Math.toRadians(0))
+                                .build()
+                );
+                break;
+        }
+
+        requestOpModeStop();
+    }
+
+//    private void collectColorSamples() {
+//        Actions.runBlocking(
+//                drive.actionBuilder(drive.pose, alliance == Alliance.BLUE)
+//                        .splineToSplineHeading(new Pose2d(17.07, -49.05, Math.toRadians(90)), Math.toRadians(-14.83))
+//                        .splineToLinearHeading(new Pose2d(48, -43, Math.toRadians(90)), Math.toRadians(67.62))
+//                        .build()
+//        );
+//
+//        setState(State.COLLECT_SPECIMEN);
+//    }
+//
+//    private void pickupSpecimen() {
+//        Actions.runBlocking(
+//                drive.actionBuilder(drive.pose, alliance == Alliance.BLUE)
+//                        .splineToLinearHeading(new Pose2d(35, -55, Math.toRadians(-90)), Math.toRadians(180))
+//                        .build()
+//        );
+//
+//        setState(State.HANG_SPECIMEN);
+//    }
 }
