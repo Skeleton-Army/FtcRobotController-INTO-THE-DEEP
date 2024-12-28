a36ddf1f706cacb0f949492ef60b0aa5b159c870
AmiT1458 <97545597+AmiT1458@users.noreply.github.com>
2024-12-28 12:30:46 +0200

    moved the apriltag action class to actions folder

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/actionClasses/Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/actionClasses/Drive.java
@@ -1,16 +1,11 @@
 package org.firstinspires.ftc.teamcode.utils.actionClasses;
 
-import androidx.annotation.NonNull;
-
-import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.Pose2d;
-import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.teamcode.roadrunner.MecanumDrive;
-import org.firstinspires.ftc.teamcode.utils.autoTeleop.Apriltag;
-import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.teamcode.utils.actions.MoveApriltag;
 
 //TODO: do stuff here sometime!!
 public class Drive {
@@ -22,38 +17,8 @@ public class Drive {
         this.drive = drive;
     }
 
-    public class MoveApriltag implements Action {
-        Apriltag apriltag = new Apriltag(hardwareMap, drive);
-        Pose2d targetPose;
-
-        public MoveApriltag(Pose2d targetPose) {
-            this.targetPose = targetPose;
-        }
-
-        @Override
-        public boolean run(@NonNull TelemetryPacket telemetryPacket) {
-            apriltag.enableApriltag();
-
-            AprilTagDetection detections = apriltag.getCurrentDetections().get(0);
-
-            // if the target position is the origin, the target position will be the Apriltag's position on the field
-            if (targetPose.equals(new Pose2d(0,0,0)))
-                this.targetPose = new Pose2d(detections.rawPose.x - 6, detections.rawPose.y - 6,0);
-
-
-            // do a spline to the target apriltag, in this case the first one that was detected
-            Actions.runBlocking(
-                    drive.actionBuilder(apriltag.getRobotPos(detections))
-                            .splineToLinearHeading(targetPose, 0)
-                            .build()
-            );
-
-            return false;
-        }
-    }
-
     public Action moveApriltag(Pose2d targetPose) {
-        return new MoveApriltag(targetPose);
+        return new MoveApriltag(targetPose, drive);
 
     }
 
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/actions/MoveApriltag.java
@@ -0,0 +1,48 @@
+package org.firstinspires.ftc.teamcode.utils.actions;
+
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+
+import androidx.annotation.NonNull;
+
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+
+import org.firstinspires.ftc.teamcode.roadrunner.MecanumDrive;
+import org.firstinspires.ftc.teamcode.utils.autoTeleop.Apriltag;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+
+public class MoveApriltag implements Action {
+    Apriltag apriltag;
+    Pose2d targetPose;
+
+    MecanumDrive drive;
+
+    public MoveApriltag(Pose2d targetPose, MecanumDrive drive) {
+        this.targetPose = targetPose;
+        this.drive = drive;
+        this.apriltag = new Apriltag(hardwareMap, drive);
+    }
+
+    @Override
+    public boolean run(@NonNull TelemetryPacket telemetryPacket) {
+        apriltag.enableApriltag();
+
+        AprilTagDetection detections = apriltag.getCurrentDetections().get(0);
+
+        // if the target position is the origin, the target position will be the Apriltag's position on the field
+        if (targetPose.equals(new Pose2d(0,0,0)))
+            this.targetPose = new Pose2d(detections.rawPose.x - 6, detections.rawPose.y - 6,0);
+
+
+        // do a spline to the target apriltag, in this case the first one that was detected
+        Actions.runBlocking(
+                drive.actionBuilder(apriltag.getRobotPos(detections))
+                        .splineToLinearHeading(targetPose, 0)
+                        .build()
+        );
+
+        return false;
+    }
+}
