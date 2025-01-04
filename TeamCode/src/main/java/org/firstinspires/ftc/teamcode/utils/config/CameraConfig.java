09a9b92409292991834fc08337bf53ac45438e95
angrybirdsFTC <angrybirdsftc@gmail.com>
2025-01-04 19:02:50 +0200

    set new values for the new camera on the main robot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/config/CameraConfig.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/config/CameraConfig.java
@@ -4,13 +4,13 @@ import com.acmerobotics.dashboard.config.Config;
 
 @Config
 public class CameraConfig {
-    public static float z = 13.125f;
+    public static float z = 12.5f;
     public static float horizontalFOV = 63.62f; // needs tuning
     public static float verticalFOV = 35.78f; // needs tuning
     public static int halfImageWidth = 320;
     public static int halfImageHeight = 180;
-    public static double offsetX = 0; //x is horizontal not IDO-style, -is when overshooting (it passes it = backwards)
-    public static double offsetY = 0;
+    public static double offsetX = 3.75; //x is horizontal not IDO-style, -is when overshooting (it passes it = backwards)
+    public static double offsetY = 7.06;
     public static double offsetHorizontal = 0;
     public static double offsetVertical = 0;
     public static float hOVERwidth = horizontalFOV / (halfImageWidth * 2);
