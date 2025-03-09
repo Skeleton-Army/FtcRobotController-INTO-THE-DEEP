e50fe8f457f07c69375645147b3fdd782e234724
angrybirdsFTC <angrybirdsftc@gmail.com>
2025-03-09 19:54:13 +0200

    New hang constants (now better)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/config/HangConfig.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/config/HangConfig.java
@@ -8,7 +8,7 @@ public class HangConfig {
 
     public static double hangPower = 1;
     public static int extendPosition = -5400;
-    public static int middlePosition = -4400;
+    public static int middlePosition = -4300;
     public static int retractPosition = 1100;
 
     public static int velocityThreshold = 100;
