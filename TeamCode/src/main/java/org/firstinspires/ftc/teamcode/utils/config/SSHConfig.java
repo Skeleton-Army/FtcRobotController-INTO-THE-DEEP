d2c02c7d23de4522ebdb67e5fceed6f70f28a8c7
angrybirdsFTC <angrybirdsftc@gmail.com>
2025-04-27 11:56:23 +0300

    jonathan cooked

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/config/SSHConfig.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.utils.config;
+
+import com.acmerobotics.dashboard.config.Config;
+
+@Config
+public class SSHConfig {
+    public static String RemoteHost;
+    public static String Username;
+    public static String Password;
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/debugging/ConceptDatalogger2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/debugging/ConceptDatalogger2.java
@@ -1,34 +1,36 @@
 package org.firstinspires.ftc.teamcode.utils.debugging;
 
-
 import com.jcraft.jsch.ChannelSftp;
 import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.JSchException;
 import com.jcraft.jsch.Session;
 import com.jcraft.jsch.SftpException;
+import org.firstinspires.ftc.teamcode.utils.config.SSHConfig;
+
+import java.io.File;
 
 public class ConceptDatalogger2 {
-    private final String RemoteHost = "192.168.1.158";
-    private final String Username = "you aint getting my username lol";
-    private final String Password = "or password";
-    //move all strings to the config.java file
 
     private ChannelSftp setupJsch() throws JSchException {
         JSch jsch = new JSch();
-        jsch.setKnownHosts("~/.ssh/known_hosts");
-        Session jschSession = jsch.getSession(Username, RemoteHost);
-        jschSession.setPassword(Password);
+        jsch.setKnownHosts("~/.shh/known_hosts");
+        Session jschSession = jsch.getSession(SSHConfig.Username, SSHConfig.RemoteHost);
+        jschSession.setPassword(SSHConfig.Password);
         jschSession.connect();
         return (ChannelSftp) jschSession.openChannel("sftp");
     }
-    public void UploadLog() throws JSchException, SftpException {
+    public void UploadLog(String LocalFile, String RemotePath, String RemoteFileName) throws JSchException, SftpException {
         ChannelSftp channelSftp = setupJsch();
         channelSftp.connect();
 
-        String LocalFile = "/home/opi3636/Videos/true.gif";
-        String RemoteLocation = "/home/username/";
+        String RemoteFile;
+        if (RemotePath.endsWith(File.separator)) {
+            RemoteFile = RemotePath + RemoteFileName;
+        } else {
+            RemoteFile = RemotePath + File.separator + RemoteFileName;
+        }
 
-        channelSftp.put(LocalFile, RemoteLocation + "true.gif");
+        channelSftp.put(LocalFile, RemoteFile);
         channelSftp.disconnect();
     }
 }
