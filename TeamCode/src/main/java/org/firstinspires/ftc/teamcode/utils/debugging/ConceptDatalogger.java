c5e9dea15f04977c647fa4ec21d78e15dbce628a
AmiT1458 <97545597+AmiT1458@users.noreply.github.com>
2025-05-03 22:14:01 +0300

    optimized memory csv datalogger

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/debugging/ConceptDatalogger.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/debugging/ConceptDatalogger.java
@@ -2,17 +2,7 @@
 This sample FTC OpMode uses methods of the Datalogger class to specify and
 collect robot data to be logged in a CSV file, ready for download and charting.
 
-For instructions, see the tutorial at the FTC Wiki:
-https://github.com/FIRST-Tech-Challenge/FtcRobotController/wiki/Datalogging
-
-
-The Datalogger class is suitable for FTC OnBot Java (OBJ) programmers.
-Its methods can be made available for FTC Blocks, by creating myBlocks in OBJ.
-
-Android Studio programmers can see instructions in the Datalogger class notes.
-
 Credit to @Windwoes (https://github.com/Windwoes).
-
 */
 
 package org.firstinspires.ftc.teamcode.utils.debugging;
@@ -27,53 +17,31 @@ import com.qualcomm.robotcore.hardware.VoltageSensor;
 import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 
 import java.io.File;
-import java.io.IOException;
 
 @TeleOp(name = "Concept Datalogger v01", group = "Datalogging")
-public class ConceptDatalogger extends LinearOpMode
-{
+public class ConceptDatalogger extends LinearOpMode {
     Datalog datalog;
     IMU imu;
     VoltageSensor battery;
-
     DcMotorEx motor;
 
     @Override
-    public void runOpMode() throws InterruptedException
-    {
-        // Get devices from the hardwareMap.
-        // If needed, change "Control Hub" to (e.g.) "Expansion Hub 1".
+    public void runOpMode() throws InterruptedException {
         battery = hardwareMap.voltageSensor.get("Control Hub");
         imu = hardwareMap.get(IMU.class, "imu");
 
-
-        /*// Create a unique filename based on date and time
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy_MM_dd_HHmmss");
-        String timestamp = LocalDateTime.now().format(dtf);
-        String filename = "datalog_" + timestamp;*/
-
-        String filename;
-
-        // You do not need to fill every field of the datalog
-        // every time you call writeLine(); those fields will simply
-        // contain the last value.
-
-        // Initialize IMU parameters
-        RevHubOrientationOnRobot.LogoFacingDirection logoDirection = RevHubOrientationOnRobot.LogoFacingDirection.UP;
-        RevHubOrientationOnRobot.UsbFacingDirection  usbDirection  = RevHubOrientationOnRobot.UsbFacingDirection.FORWARD;
-
-        RevHubOrientationOnRobot orientationOnRobot = new RevHubOrientationOnRobot(logoDirection, usbDirection);
-        IMU.Parameters parameters = new IMU.Parameters(orientationOnRobot);
-        imu.initialize(parameters);
+        // IMU initialization
+        RevHubOrientationOnRobot orientationOnRobot = new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                RevHubOrientationOnRobot.UsbFacingDirection.FORWARD);
+        imu.initialize(new IMU.Parameters(orientationOnRobot));
 
         telemetry.setMsTransmissionInterval(50);
 
-
+        // File setup
         File downloadDir = new File("/sdcard/FIRST/Datalogs");
         int index = 1;
         File logFile;
-
-        // Find the next available filename
         do {
             logFile = new File(downloadDir, "my_log_" + index + ".csv");
             index++;
@@ -82,8 +50,7 @@ public class ConceptDatalogger extends LinearOpMode
         telemetry.addLine("Log file name: " + logFile.getName());
         telemetry.update();
 
-        filename = logFile.getName();
-        datalog = new Datalog(filename);
+        datalog = new Datalog(logFile.getName());
 
         datalog.opModeStatus.set("INIT");
         datalog.battery.set(battery.getVoltage());
@@ -93,25 +60,17 @@ public class ConceptDatalogger extends LinearOpMode
 
         datalog.opModeStatus.set("RUNNING");
 
-        for (int i = 0; opModeIsActive(); i++)
-        {
-            // Note that the order in which we set datalog fields
-            // does *not* matter! The order is configured inside
-            // the Datalog class constructor.
-
+        for (int i = 0; opModeIsActive(); i++) {
             datalog.loopCounter.set(i);
             datalog.battery.set(battery.getVoltage());
 
             YawPitchRollAngles orientation = imu.getRobotYawPitchRollAngles();
-
             datalog.yaw.set(orientation.getYaw());
             datalog.pitch.set(orientation.getPitch());
             datalog.roll.set(orientation.getRoll());
 
-            // The logged timestamp is taken when writeLine() is called.
             datalog.writeLine();
 
-            // Datalog fields are stored as text only; do not format here.
             telemetry.addData("Yaw", datalog.yaw);
             telemetry.addData("Pitch", datalog.pitch);
             telemetry.addData("Roll", datalog.roll);
@@ -121,53 +80,23 @@ public class ConceptDatalogger extends LinearOpMode
             telemetry.addData("Battery", datalog.battery);
 
             for (VoltageSensor sensor : hardwareMap.getAll(VoltageSensor.class)) {
-                telemetry.addData("Voltage Sensor Name: ", sensor.getDeviceName());
-                telemetry.addData("Voltage: ", sensor.getVoltage());
+                telemetry.addData("Voltage Sensor Name", sensor.getDeviceName());
+                telemetry.addData("Voltage", sensor.getVoltage());
             }
-
             telemetry.update();
 
             sleep(20);
         }
 
         if (isStopRequested()) {
-
             sleep(250);
-//
-//            ProcessBuilder builder = new ProcessBuilder();
-//
-//            builder.directory();
-//            builder.command("cmd.exe", "/c", "adb pull /sdcard/FIRST/Datalogs/ " + filename + "C:/Users/User/Desktop");
-//
-//            builder.command("python FtcRobotController-INTO-THE-DEEP/debugging/copy_csv_datalog.py");
-
-//            try {
-//                Runtime.getRuntime().exec("adb pull /sdcard/FIRST/Datalogs/"+ filename + " C:/Users/User/Desktop");
-//
-//                // runs the python script after the file is at Desktop
-//                Runtime.getRuntime().exec("python FtcRobotController-INTO-THE-DEEP/debugging/copy_csv_datalog.py");
-//            } catch (IOException e) {
-//                throw new RuntimeException(e);
-//            }
-
+            // File operations are removed for OnBot Java compatibility
         }
-
-        /*
-         * The datalog is automatically closed and flushed to disk after
-         * the OpMode ends - no need to do that manually :')
-         */
     }
 
-    /*
-     * This class encapsulates all the fields that will go into the datalog.
-     */
-    public static class Datalog
-    {
-        // The underlying datalogger object - it cares only about an array of loggable fields
+    public static class Datalog {
         private final Datalogger datalogger;
 
-        // These are all of the fields that we want in the datalog.
-        // Note that order here is NOT important. The order is important in the setFields() call below
         public Datalogger.GenericField opModeStatus = new Datalogger.GenericField("OpModeStatus");
         public Datalogger.GenericField loopCounter  = new Datalogger.GenericField("Loop Counter");
         public Datalogger.GenericField yaw          = new Datalogger.GenericField("Yaw");
@@ -175,20 +104,10 @@ public class ConceptDatalogger extends LinearOpMode
         public Datalogger.GenericField roll         = new Datalogger.GenericField("Roll");
         public Datalogger.GenericField battery      = new Datalogger.GenericField("Battery");
 
-        public Datalog(String name)
-        {
-            // Build the underlying datalog object
-            datalogger = new Datalogger.Builder()
-
-                    // Pass through the filename
+        public Datalog(String name) {
+            datalogger = Datalogger.builder()
                     .setFilename(name)
-
-                    // Request an automatic timestamp field
                     .setAutoTimestamp(Datalogger.AutoTimestamp.DECIMAL_SECONDS)
-
-                    // Tell it about the fields we care to log.
-                    // Note that order *IS* important here! The order in which we list
-                    // the fields is the order in which they will appear in the log.
                     .setFields(
                             opModeStatus,
                             loopCounter,
@@ -200,10 +119,7 @@ public class ConceptDatalogger extends LinearOpMode
                     .build();
         }
 
-        // Tell the datalogger to gather the values of the fields
-        // and write a new line in the log.
-        public void writeLine()
-        {
+        public void writeLine() {
             datalogger.writeLine();
         }
     }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/debugging/Datalogger.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/debugging/Datalogger.java
@@ -1,334 +1,119 @@
-/*
-This Datalogger class is provided for FTC OnBot Java (OBJ) programmers.
-
-Most users will not need to edit this class; its methods are called
-from a user's OpMode such as ConceptDatalogger.java or a revised version.
-That OpMode specifies and collects data to be logged in a CSV file,
-ready for download and charting.
-
-For instructions, see the tutorial at the FTC Wiki:
-https://github.com/FIRST-Tech-Challenge/FtcRobotController/wiki/Datalogging
-
-
-Android Studio programmers can change the destination filepath at Line 295,
-From: "/sdcard/FIRST/java/src/Datalogs/%s.txt"
-To:   "/sdcard/FIRST/Datalogs/%s.csv"
-This change presumes OnBot Java will not be used to preview or download datalogs;
-they will instead be manually transferred from the RC device.
-
-Credit to @Windwoes (https://github.com/Windwoes).
-
-*/
-
-
 package org.firstinspires.ftc.teamcode.utils.debugging;
 
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.eventloop.opmode.OpModeManagerNotifier;
-
-import com.qualcomm.robotcore.eventloop.opmode.OpModeManagerImpl;
-import org.firstinspires.ftc.robotcore.internal.system.AppUtil;
-
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Datalogger
-{
-    private LoggableField[] fields;
-    private BufferedCsvWriter bufferedCsvWriter;
+public class Datalogger {
+    public enum AutoTimestamp { NONE, DECIMAL_SECONDS }
 
-    /*
-     * NOTE: We cannot simply pass `new OpModeNotifications()` inline to the call
-     * to register the listener, because the SDK stores the list of listeners in
-     * a WeakReference set. This causes the object to be garbage collected because
-     * nothing else is holding a reference to it.
-     */
-    private OpModeNotifications opModeNotifications = new OpModeNotifications();
-
-    private Datalogger(BufferedCsvWriter bufferedCsvWriter, LoggableField[] fields)
-    {
-        this.bufferedCsvWriter = bufferedCsvWriter;
-        this.fields = fields;
-
-        OpModeManagerImpl.getOpModeManagerOfActivity(AppUtil.getInstance().getActivity()).registerListener(opModeNotifications);
-
-        writeHeader();
+    public interface LoggableField {
+        String getName();
+        String getValue();
     }
 
-    private class OpModeNotifications implements OpModeManagerNotifier.Notifications
-    {
-        @Override
-        public void onOpModePostStop(OpMode opMode)
-        {
-            close();
-            OpModeManagerImpl.getOpModeManagerOfActivity(AppUtil.getInstance().getActivity()).unregisterListener(this);
+    private String filename;
+    private AutoTimestamp timestampMode;
+    private List<LoggableField> fields = new ArrayList<>();
+    private BufferedWriter writer;
+    private long startTime;
+
+    private Datalogger() {}
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static class Builder {
+        private final Datalogger instance = new Datalogger();
+
+        public Builder setFilename(String name) {
+            instance.filename = "/sdcard/FIRST/Datalogs/" + name;
+            return this;
         }
 
-        @Override
-        public void onOpModePreInit(OpMode opMode) {}
+        public Builder setAutoTimestamp(AutoTimestamp mode) {
+            instance.timestampMode = mode;
+            return this;
+        }
 
-        @Override
-        public void onOpModePreStart(OpMode opMode) {}
-    }
-
-    private void writeHeader()
-    {
-        StringBuilder stringBuilder = new StringBuilder();
-
-        for (int i = 0; i < fields.length; i++)
-        {
-            stringBuilder.append(fields[i].name);
-            if (i < fields.length-1)
-            {
-                stringBuilder.append(",");
+        public Builder setFields(LoggableField... logFields) {
+            for (LoggableField field : logFields) {
+                instance.fields.add(field);
             }
+            return this;
         }
 
-        try
-        {
-            bufferedCsvWriter.writeLine(stringBuilder.toString());
-        }
-        catch (IOException e)
-        {
-            e.printStackTrace();
-            throw new RuntimeException("Unable to initialize datalogger");
-        }
-    }
+        public Datalogger build() {
+            try {
+                instance.writer = new BufferedWriter(new FileWriter(instance.filename));
+                instance.startTime = System.nanoTime();
 
-    public void writeLine()
-    {
-        StringBuilder stringBuilder = new StringBuilder();
-
-        for (int i = 0; i < fields.length; i++)
-        {
-            fields[i].writeToBuffer(stringBuilder);
-            if (i < fields.length-1)
-            {
-                stringBuilder.append(",");
+                // Write CSV header
+                List<String> headers = new ArrayList<>();
+                if (instance.timestampMode != AutoTimestamp.NONE) {
+                    headers.add("Timestamp");
+                }
+                for (LoggableField field : instance.fields) {
+                    headers.add(field.getName());
+                }
+                instance.writer.write(String.join(",", headers));
+                instance.writer.newLine();
+                instance.writer.flush();
+            } catch (IOException e) {
+                throw new RuntimeException("Failed to open log file", e);
             }
-        }
 
-        try
-        {
-            bufferedCsvWriter.writeLine(stringBuilder.toString());
-        }
-        catch (IOException e)
-        {
-            e.printStackTrace();
-            throw new RuntimeException("Error writing datalog line");
+            return instance;
         }
     }
 
-    private void close()
-    {
-        try
-        {
-            bufferedCsvWriter.close();
-        }
-        catch (IOException e)
-        {
-            e.printStackTrace();
+    public void writeLine() {
+        try {
+            List<String> values = new ArrayList<>();
+            if (timestampMode == AutoTimestamp.DECIMAL_SECONDS) {
+                double seconds = (System.nanoTime() - startTime) / 1e9;
+                values.add(String.format("%.3f", seconds));
+            }
+
+            for (LoggableField field : fields) {
+                values.add(field.getValue());
+            }
+
+            writer.write(String.join(",", values));
+            writer.newLine();
+            writer.flush();
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to write datalog line", e);
         }
     }
 
-    public static abstract class LoggableField
-    {
-        protected final String name;
+    public static class GenericField implements LoggableField {
+        private final String name;
+        private String value = "";
 
-        public LoggableField(String name)
-        {
+        public GenericField(String name) {
             this.name = name;
         }
 
-        public abstract void writeToBuffer(StringBuilder out);
-    }
+        public void set(double value) {
+            this.value = Double.toString(value);
+        }
 
-    public static class GenericField extends LoggableField
-    {
-        private String str = "";
-        private static final String STR_FALSE = "false";
-        private static final String STR_TRUE = "true";
-
-        public GenericField(String name)
-        {
-            super(name);
+        public void set(String value) {
+            this.value = value;
         }
 
         @Override
-        public void writeToBuffer(StringBuilder out)
-        {
-            out.append(str);
-        }
-
-        public void set(String string)
-        {
-            str = string;
-        }
-
-        public void set(String format, Object... args)
-        {
-            str = String.format(format, args);
-        }
-
-        public void set(int val)
-        {
-            str = Integer.toString(val);
-        }
-
-        public void set(boolean val)
-        {
-            str = val ? STR_TRUE : STR_FALSE;
-        }
-
-        public void set(byte val)
-        {
-            str = String.format("0x%x", val);
-        }
-
-        public void set(float val)
-        {
-            str = String.format("%.3f", val);
-        }
-
-        // 6-7-22 Add overloaded method with optional format parameter.
-        public void set(String valFormat, float val)
-        {
-            str = String.format(valFormat, val);
-        }
-
-        public void set(double val)
-        {
-            str = String.format("%.3f", val);
-        }
-
-        // 6-7-22 Add overloaded method with optional format parameter.
-        public void set(String valFormat, double val)
-        {
-            str = String.format(valFormat, val);
-        }
-
-        // 6-7-22  Added this method so user OpMode telemetry can display 
-        // field contents (instead of memory address).
-        @Override
-        public String toString()
-        {
-            return str;
-        }
-    }
-
-    private static class TimestampField extends LoggableField
-    {
-        private long tRef;
-        private final DecimalFormat timeFmt = new DecimalFormat("000.000");
-
-        public TimestampField(String name)
-        {
-            super(name);
-            tRef = System.currentTimeMillis();
-        }
-
-        public void resetRef()
-        {
-            tRef = System.currentTimeMillis();
+        public String getName() {
+            return name;
         }
 
         @Override
-        public void writeToBuffer(StringBuilder out)
-        {
-            long deltaMs = System.currentTimeMillis() - tRef;
-            float delta = deltaMs / 1000f;
-            out.append(timeFmt.format(delta));
-        }
-    }
-
-    public enum AutoTimestamp
-    {
-        DECIMAL_SECONDS,
-        NONE
-    }
-
-    public static class Builder
-    {
-        private String filename;
-        private LoggableField[] fields;
-        private AutoTimestamp autoTimestamp;
-
-        public Builder setFilename(String filename)
-        {
-            this.filename = filename;
-            return this;
-        }
-
-        public Builder setFields(LoggableField... fields)
-        {
-            this.fields = fields;
-            return this;
-        }
-
-        public Builder setAutoTimestamp(AutoTimestamp autoTimestamp)
-        {
-            this.autoTimestamp = autoTimestamp;
-            return this;
-        }
-
-        public Datalogger build()
-        {
-            if (filename == null) throw new RuntimeException("Filename must not be null!");
-            if (filename.endsWith(".csv")) filename = filename.replace(".csv", "");
-            if (fields == null) throw new RuntimeException("Fields must not be null!");
-            if (fields.length == 0) throw new RuntimeException("Fields must be non-zero length!");
-            if (autoTimestamp == null) throw new RuntimeException("AutoTimestamp must not be null!");
-
-            if (autoTimestamp == AutoTimestamp.DECIMAL_SECONDS)
-            {
-                LoggableField[] tmp = new LoggableField[fields.length+1];
-                tmp[0] = new TimestampField("Timestamp");
-                System.arraycopy(fields, 0, tmp, 1, fields.length);
-                fields = tmp;
-            }
-
-            try
-            {
-                BufferedCsvWriter bufferedCsvWriter = new BufferedCsvWriter(String.format("/sdcard/FIRST/Datalogs/%s.csv", filename));
-                return new Datalogger(bufferedCsvWriter, fields);
-            }
-            catch (IOException e)
-            {
-                e.printStackTrace();
-                throw new RuntimeException("Unable to create output file handle :(");
-            }
-        }
-    }
-
-    private static class BufferedCsvWriter
-    {
-        private FileWriter fileWriter;
-        private BufferedWriter bufferedWriter;
-
-        public BufferedCsvWriter(String filepath) throws IOException
-        {
-            File tmp = new File(filepath);
-            if (!tmp.exists())
-            {
-                tmp.getParentFile().mkdirs();
-            }
-
-            fileWriter = new FileWriter(filepath, false);
-            bufferedWriter = new BufferedWriter(fileWriter);
-        }
-
-        public void writeLine(String line) throws IOException
-        {
-            bufferedWriter.write(line);
-            bufferedWriter.newLine();
-        }
-
-        public void close() throws IOException
-        {
-            bufferedWriter.close();
+        public String getValue() {
+            return value;
         }
     }
 }
