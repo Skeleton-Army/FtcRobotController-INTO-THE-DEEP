eec8f460f44c2b24ff175d20c9184aa3b038b972
angrybirdsFTC <angrybirdsftc@gmail.com>
2025-05-27 18:52:15 +0300

    tuned new values for intake

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/config/IntakeConfig.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/config/IntakeConfig.java
@@ -11,17 +11,17 @@ public class IntakeConfig {
     public static String rotationName = "rotation";
 
     public static double motorPower = 1;
-    public static int extendPosition = -450;
+    public static int extendPosition = -535;
     public static int retractPosition = 0;
 
     public static int velocityThreshold = 500;
     public static double startThreshold = 0.5;
 
-    public static double clawClosed = 0.6;
-    public static double clawOpen = 0.33;
+    public static double clawClosed = 0.86;
+    public static double clawOpen = 0.5;
     public static double extraOpenClaw = 0;
 
-    public static double wristExtend = 1;
+    public static double wristExtend = 0.96;
     public static double wristRetract = 0.35;
     public static double wristMiddle = 0.6;
     public static double wristReady = 0.75;
